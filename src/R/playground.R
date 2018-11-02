b54198ea0a58a1333ac4ba1b6ed0f114b625d4cc
harrisonzhu508 <harrisonzhu5080@gmail.com>
2018-11-02 18:48:56 +0000

    add baselines

---
+++ src/R/playground.R
@@ -0,0 +1,12 @@
+# Citadel UK Datathon challenge
+# Authors: Harrison Zhu, Benjamin Hillyard, 
+# Source file for code
+
+library(utils)
+library(stats)
+
+# Read in the datasets
+data <- read.csv("./dataset.csv")
+
+
+#
